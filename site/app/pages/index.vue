93bc6618a32ac4ad206da1f82a0d71de4c4eba3b
vvsubashshareplay <venkat@sharplay.tv>
2025-02-08 19:42:45 -0800

    second commit

--- site/app/pages/index.vue
+++ site/app/pages/index.vue
@@ -1,45 +1,39 @@
 <script setup lang="ts">
-const runtimeConfig = useRuntimeConfig()
-const colors = ['#f87171', '#fb923c', '#fbbf24', '#facc15', '#a3e635', '#4ade80', '#34d399', '#2dd4bf', '#22d3ee', '#38bdf8', '#60a5fa', '#818cf8', '#a78bfa', '#c084fc', '#e879f9', '#f472b6', '#fb7185']
-const color = useState('color', () => colors[Math.floor(Math.random() * colors.length)])
+
 </script>
 
 <template>
-  <div class="centered">
-    <h1 :style="{ color }">
-      {{ runtimeConfig.public.helloText }}
-    </h1>
-    <NuxtLink to="/" external>
-      refresh
-    </NuxtLink>
-  </div>
+  <section>
+    <h1>Hello World</h1>
+    <p>This is a test page</p>
+    <div class="card">
+      <div class="flex width">
+        <fieldset class="width">
+          <label>Point</label>
+          <select>
+            <option value="1">Indus</option>
+            <option value="2">Main Gate</option>
+            <option value="3">Gangavaram</option>
+          </select>
+        </fieldset>
+        <fieldset class="width">
+          <label>Shift</label>
+          <select>
+            <option value="1">A</option>
+            <option value="2">B</option>
+            <option value="3">C</option>
+          </select>
+        </fieldset>
+      </div>
+      <fieldset>
+        <label>Name</label>
+        <input type="text" />
+      </fieldset>
+      <button class="widht">Submit</button>
+    </div>
+  </section>
 </template>
 
 <style scoped>
-.centered {
-  position: absolute;
-  width: 100%;
-  text-align: center;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  margin: 0;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}
-h1 {
-  font-size: 32px;
-}
-@media (min-width: 768px) {
-  h1 {
-    font-size: 64px;
-  }
-}
-a {
-  color: #888;
-  text-decoration: none;
-  font-size: 18px;
-}
-a:hover {
-  text-decoration: underline;
-}
+
 </style>
